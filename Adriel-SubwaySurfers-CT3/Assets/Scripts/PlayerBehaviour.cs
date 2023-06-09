99f144450a8d5f0ab5739afb7b4e6fc7c8eaebc8
unknown <adrielestoller@gmail.com>
2023-06-09 18:07:57 -0300

    Arquivos Adriel

---
+++ Adriel-SubwaySurfers-CT3/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool left, center, right, isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        left = true;
+        right = true;
+        center = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            moveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moveRight();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded || Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            jump();
+        }
+    }
+
+    private void moveLeft()
+    {
+        if (left && !center)
+        {
+            transform.position = new Vector3(-3f, 0f, 0f);
+
+            left = false;
+            center = true;
+            right = true;
+        }
+        else if (center && !right)
+        {
+            transform.position = new Vector3(0f, 0f, 0f);
+
+            left = true;
+            center = false;
+            right = true;
+        }
+    }
+
+    private void moveRight()
+    {
+        if (right && !center)
+        {
+            transform.position = new Vector3(3f, 0f, 0f);
+
+            left = true;
+            center = true;
+            right = false;
+        }
+        else if (center && !left)
+        {
+            transform.position = new Vector3(0f, 0f, 0f);
+
+            left = true;
+            center = false;
+            right = true;
+        }
+    }
+
+    private void jump()
+    {
+        rb.AddForce(new Vector3(0f, 7f, 0f), ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
