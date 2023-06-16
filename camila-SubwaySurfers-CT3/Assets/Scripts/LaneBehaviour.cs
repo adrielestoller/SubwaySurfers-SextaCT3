eefa8d5d3bcac57bc3fe57007c32be9ec17a9953
Aluno Ctrlplay <adrielestoller@gmail.com>
2023-06-16 18:01:38 -0300

    Alterações dia 16/06

---
+++ camila-SubwaySurfers-CT3/Assets/Scripts/LaneBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneBehaviour : MonoBehaviour
+
+
+{
+ private float speed = 3f;   
+
+
+    void Start()
+    {
+        Destroy(this.gameObject,10f);
+    }
+
+    
+    void Update()
+    {
+        transform.position -= new Vector3 (0f, 0f, speed) * Time.deltaTime;
+    }
+}
